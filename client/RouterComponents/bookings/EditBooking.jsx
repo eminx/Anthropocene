6eadede74c2559f9fde3e7ae9d56ad611e24f172
Emin Durak <emin@tuta.io>
2019-09-12 16:46:31 +0300

    added some error handlers

--- client/RouterComponents/bookings/EditBooking.jsx
+++ client/RouterComponents/bookings/EditBooking.jsx
@@ -1,8 +1,8 @@
-import { Meteor } from 'meteor/meteor';
-import { Link } from 'react-router-dom';
-import React from 'react';
-import CreateBookingForm from '../../UIComponents/CreateBookingForm';
-import ModalArticle from '../../UIComponents/ModalArticle';
+import { Meteor } from 'meteor/meteor'
+import { Link } from 'react-router-dom'
+import React from 'react'
+import CreateBookingForm from '../../UIComponents/CreateBookingForm'
+import ModalArticle from '../../UIComponents/ModalArticle'
 import {
   Row,
   Col,
@@ -13,19 +13,19 @@ import {
   Button,
   Switch,
   Divider
-} from 'antd/lib';
-import { Redirect } from 'react-router-dom';
+} from 'antd/lib'
+import { Redirect } from 'react-router-dom'
 
 const successEditMessage = isDeleted => {
   if (isDeleted) {
-    message.success('The booking is successfully deleted', 4);
+    message.success('The booking is successfully deleted', 4)
   } else {
-    message.success('Your booking is successfully updated', 6);
+    message.success('Your booking is successfully updated', 6)
   }
-};
+}
 
 const sideNote =
-  "Please check if a corresponding time and space is not taken already. \n It is your responsibility to make sure that there's no overlapping bookings.";
+  "Please check if a corresponding time and space is not taken already. \n It is your responsibility to make sure that there's no overlapping bookings."
 
 class EditBooking extends React.Component {
   state = {
@@ -42,107 +42,107 @@ class EditBooking extends React.Component {
     isPublicActivity: false,
     isBookingsDisabled: false,
     numberOfRecurrence: 0
-  };
-
-  componentDidMount() {
-    this.setPublicActivity();
-    this.setNumberOfRecurrence();
   }
 
-  componentDidUpdate(prevProps, prevState) {
+  componentDidMount () {
+    this.setPublicActivity()
+    this.setNumberOfRecurrence()
+  }
+
+  componentDidUpdate (prevProps, prevState) {
     if (!prevProps.gatheringData) {
-      this.setPublicActivity();
+      this.setPublicActivity()
     }
 
     if (!prevProps.gatheringData) {
-      this.setNumberOfRecurrence();
+      this.setNumberOfRecurrence()
     }
   }
 
   setUploadableImage = e => {
-    const theImageFile = e.file.originFileObj;
-    const reader = new FileReader();
-    reader.readAsDataURL(theImageFile);
+    const theImageFile = e.file.originFileObj
+    const reader = new FileReader()
+    reader.readAsDataURL(theImageFile)
     reader.addEventListener(
       'load',
       () => {
         this.setState({
           uploadableImage: theImageFile,
           uploadableImageLocal: reader.result
-        });
+        })
       },
       false
-    );
-  };
+    )
+  }
 
   setPublicActivity = () => {
-    const { gatheringData } = this.props;
+    const { gatheringData } = this.props
     if (!gatheringData) {
-      return;
+      return
     }
 
     this.setState({
       isPublicActivity: gatheringData.isPublicActivity,
       isBookingsDisabled: Boolean(gatheringData.isBookingsDisabled)
-    });
-  };
+    })
+  }
 
   setNumberOfRecurrence = () => {
-    const { gatheringData } = this.props;
+    const { gatheringData } = this.props
     if (!gatheringData) {
-      return;
+      return
     }
     this.setState({
       numberOfRecurrence: gatheringData.datesAndTimes.length
-    });
-  };
+    })
+  }
 
   addRecurrence = () => {
     this.setState({
       numberOfRecurrence: this.state.numberOfRecurrence + 1
-    });
-  };
+    })
+  }
 
   removeRecurrence = index => {
     this.setState({
       numberOfRecurrence: this.state.numberOfRecurrence + 1
-    });
-  };
+    })
+  }
 
   registerGatheringLocally = values => {
-    values.authorName = this.props.currentUser.username || 'emo';
+    values.authorName = this.props.currentUser.username || 'emo'
     this.setState({
       values: values,
       modalConfirm: true
-    });
-  };
+    })
+  }
 
   uploadImage = () => {
-    this.setState({ isLoading: true });
+    this.setState({ isLoading: true })
 
-    const { uploadableImage } = this.state;
+    const { uploadableImage } = this.state
 
     if (!uploadableImage) {
-      console.log('No uploadable image');
-      this.updateBooking();
-      return;
+      console.log('No uploadable image')
+      this.updateBooking()
+      return
     }
 
-    const upload = new Slingshot.Upload('activityImageUpload');
+    const upload = new Slingshot.Upload('activityImageUpload')
 
     upload.send(uploadableImage, (error, downloadUrl) => {
       if (error) {
-        console.error('Error uploading:', error);
+        console.error('Error uploading:', error)
       } else {
         this.setState(
           {
             uploadedImage: downloadUrl
           },
           () => this.updateBooking()
-        );
+        )
       }
-    });
-  };
+    })
+  }
 
   updateBooking = () => {
     const {
@@ -150,13 +150,13 @@ class EditBooking extends React.Component {
       isPublicActivity,
       isBookingsDisabled,
       uploadedImage
-    } = this.state;
-    const { gatheringData } = this.props;
+    } = this.state
+    const { gatheringData } = this.props
 
-    values.isPublicActivity = isPublicActivity;
-    values.isBookingsDisabled = isBookingsDisabled;
+    values.isPublicActivity = isPublicActivity
+    values.isBookingsDisabled = isBookingsDisabled
 
-    const imageUrl = uploadedImage || gatheringData.imageUrl;
+    const imageUrl = uploadedImage || gatheringData.imageUrl
 
     Meteor.call(
       'updateBooking',
@@ -165,76 +165,81 @@ class EditBooking extends React.Component {
       imageUrl,
       (error, respond) => {
         if (error) {
+          console.log(error.message)
+          console.log(error.reason)
+          console.log(error)
+          message.error(error.message)
+          message.error(error.reason)
           this.setState({
             isLoading: false,
             isError: true
-          });
+          })
         } else {
           this.setState({
             isLoading: false,
             newBookingId: respond,
             isSuccess: true
-          });
+          })
         }
       }
-    );
-  };
+    )
+  }
 
-  hideModal = () => this.setState({ modalConfirm: false });
-  showModal = () => this.setState({ modalConfirm: true });
+  hideModal = () => this.setState({ modalConfirm: false })
+  showModal = () => this.setState({ modalConfirm: true })
 
-  hideDeleteModal = () => this.setState({ isDeleteModalOn: false });
-  showDeleteModal = () => this.setState({ isDeleteModalOn: true });
+  hideDeleteModal = () => this.setState({ isDeleteModalOn: false })
+  showDeleteModal = () => this.setState({ isDeleteModalOn: true })
 
   deleteBooking = () => {
-    const bookingId = this.props.gatheringData._id;
+    const bookingId = this.props.gatheringData._id
 
     Meteor.call('deleteBooking', bookingId, (error, respond) => {
       if (error) {
         this.setState({
           isLoading: false,
           isError: true
-        });
+        })
       } else {
         this.setState({
           isLoading: false,
           isSuccess: true
-        });
+        })
       }
-    });
-  };
+    })
+  }
 
   handlePublicActivitySwitch = value => {
     this.setState({
       isPublicActivity: value
-    });
-  };
+    })
+  }
 
   handleDisableBookingsSwitch = value => {
     this.setState({
       isBookingsDisabled: value
-    });
-  };
+    })
+  }
 
   handleConfirmModal = () => {
-    const { isPublicActivity, uploadableImage } = this.state;
+    const { isPublicActivity, uploadableImage } = this.state
     if (isPublicActivity && uploadableImage) {
-      this.uploadImage();
+      this.uploadImage()
     } else {
-      this.updateBooking();
+      this.updateBooking()
     }
-  };
+  }
 
-  render() {
+  render () {
     if (!this.props.currentUser) {
       return (
         <div style={{ maxWidth: 600, margin: '0 auto' }}>
           <Alert
-            message="You have to signin to create a booking."
-            type="error"
+            message='You have to signin to create a booking.'
+            type='error'
           />
         </div>
-      );
+      )
     }
 
     const {
@@ -247,19 +252,19 @@ class EditBooking extends React.Component {
       isPublicActivity,
       isBookingsDisabled,
       numberOfRecurrence
-    } = this.state;
+    } = this.state
 
-    const { gatheringData, currentUser } = this.props;
+    const { gatheringData, currentUser } = this.props
 
     if (isSuccess) {
-      successEditMessage(isDeleteModalOn);
+      successEditMessage(isDeleteModalOn)
       if (isDeleteModalOn) {
-        return <Redirect to="/calendar" />;
+        return <Redirect to='/calendar' />
       }
       if (isPublicActivity) {
-        return <Redirect to={`/activity/${gatheringData._id}`} />;
+        return <Redirect to={`/activity/${gatheringData._id}`} />
       } else {
-        return <Redirect to="/calendar" />;
+        return <Redirect to='/calendar' />
       }
     }
 
@@ -268,7 +273,7 @@ class EditBooking extends React.Component {
         {gatheringData && (
           <div style={{ marginBottom: 12 }}>
             <Link to={`/activity/${gatheringData._id}`}>
-              <Button icon="arrow-left">{gatheringData.title}</Button>
+              <Button icon='arrow-left'>{gatheringData.title}</Button>
             </Link>
           </div>
         )}
@@ -280,16 +285,16 @@ class EditBooking extends React.Component {
             {gatheringData &&
               currentUser &&
               gatheringData.authorId === currentUser._id && (
-                <div
-                  style={{
-                    display: 'flex',
-                    justifyContent: 'flex-end',
-                    padding: 12
-                  }}
-                >
-                  <Button onClick={this.showDeleteModal}>Delete</Button>
-                </div>
-              )}
+              <div
+                style={{
+                  display: 'flex',
+                  justifyContent: 'flex-end',
+                  padding: 12
+                }}
+              >
+                <Button onClick={this.showDeleteModal}>Delete</Button>
+              </div>
+            )}
 
             <Row style={{ marginBottom: 12 }}>
               <Col md={12}>
@@ -329,7 +334,7 @@ class EditBooking extends React.Component {
           </Col>
           <Col xs={24} sm={24} md={8}>
             <Affix offsetTop={50}>
-              <Alert message={sideNote} type="warning" showIcon />
+              <Alert message={sideNote} type='warning' showIcon />
             </Affix>
           </Col>
         </Row>
@@ -337,28 +342,28 @@ class EditBooking extends React.Component {
           <ModalArticle
             item={values}
             isLoading={isLoading}
-            title="Overview The Information"
+            title='Overview The Information'
             visible={modalConfirm}
             onOk={this.handleConfirmModal}
             onCancel={this.hideModal}
-            okText="Confirm"
-            cancelText="Go back and edit"
+            okText='Confirm'
+            cancelText='Go back and edit'
           />
         )}
 
         <Modal
-          title="Confirm"
+          title='Confirm'
           visible={isDeleteModalOn}
           onOk={this.deleteBooking}
           onCancel={this.hideDeleteModal}
-          okText="Yes, delete"
-          cancelText="Cancel"
+          okText='Yes, delete'
+          cancelText='Cancel'
         >
           Are you sure you want to delete this booking?
         </Modal>
       </div>
-    );
+    )
   }
 }
 
-export default EditBooking;
+export default EditBooking
